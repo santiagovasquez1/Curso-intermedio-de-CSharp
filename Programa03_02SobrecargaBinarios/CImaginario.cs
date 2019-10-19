f26cbeefb3ea6f2bf4edd3237691366aa07e85d4
svasquez <svasquez@controldiseno.com>
2019-10-19 18:28:28 -0500

    Sobrecarga de operadores en las clases

---
+++ Programa03_02SobrecargaBinarios/CImaginario.cs
@@ -0,0 +1,108 @@
+﻿namespace Programa03_02SobrecargaBinarios
+{
+    internal class CImaginario
+    {
+        private double entero;
+        private double imaginario;
+
+        public CImaginario(double pEntero, double pImaginario)
+        {
+            entero = pEntero;
+            imaginario = pImaginario;
+        }
+
+        //propiedades
+        public double Entero { get { return entero; } set { entero = value; } }
+
+        public double Imaginario { get { return imaginario; } set { imaginario = value; } }
+
+        public override string ToString()
+        {
+            if (imaginario < 0)
+            {
+                return string.Format("({0} {1}i)", entero, imaginario);
+            }
+            else
+            {
+                return string.Format("({0} + {1}i)", entero, imaginario);
+            }
+        }
+
+        //Sobrecarga de operadores binarios
+        public static CImaginario operator +(CImaginario i1, CImaginario i2)
+        {
+            double re = i1.entero + i2.entero;
+            double ri = i1.imaginario + i2.imaginario;
+            CImaginario temp = new CImaginario(re, ri);
+            return temp;
+        }
+
+        public static CImaginario operator -(CImaginario i1, CImaginario i2)
+        {
+            double re = i1.entero - i2.entero;
+            double ri = i1.imaginario - i2.imaginario;
+            CImaginario temp = new CImaginario(re, ri);
+            return temp;
+        }
+
+        public static CImaginario operator *(CImaginario i1, CImaginario i2)
+        {
+            double re = i1.entero * i2.entero - i1.imaginario * i2.imaginario;
+            double ri = i1.entero * i2.imaginario + i1.imaginario * i2.entero;
+            CImaginario temp = new CImaginario(re, ri);
+            return temp;
+        }
+
+        public static CImaginario operator /(CImaginario i1, CImaginario i2)
+        {
+            double re = (i1.entero * i2.entero + i1.imaginario * i2.imaginario)
+                        / (i2.entero * i2.entero + i2.imaginario * i2.imaginario);
+
+            double ri = (i1.imaginario * i2.entero - i1.entero * i2.imaginario)
+                        / (i2.entero * i2.entero + i2.imaginario * i2.imaginario);
+
+            CImaginario temp = new CImaginario(re, ri);
+            return temp;
+        }
+
+        //Sobrecarga de operadores unuarios
+        public static CImaginario operator ++(CImaginario i1)
+        {
+            CImaginario temp = new CImaginario(i1.entero + 1, i1.imaginario + 1);
+            return temp;
+        }
+
+        public static CImaginario operator --(CImaginario i1)
+        {
+            CImaginario temp = new CImaginario(i1.entero - 1, i1.imaginario - 1);
+            return temp;
+        }
+
+        //Sobrecarga de operador de igualdad y desigualdad
+
+        public override bool Equals(object obj)
+        {
+            //Verificacion de que el tipo sea el correcto
+            if (obj is CImaginario)
+            {
+                CImaginario temp = (CImaginario)obj;
+
+                //Comparamos la igualdad
+                if (imaginario == temp.imaginario && entero == temp.entero)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator ==(CImaginario i1, CImaginario i2)
+        {
+            return i1.Equals(i2);
+        }
+
+        public static bool operator !=(CImaginario i1, CImaginario i2)
+        {
+            return !i1.Equals(i2); //Negacion de equals
+        }
+    }
+}
---
+++ Programa03_02SobrecargaBinarios/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa03_02SobrecargaBinarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CImaginario im1 = new CImaginario(1, 2);
+            CImaginario im2 = new CImaginario(3, 4);
+            CImaginario imr,imr1,imr2,imr3,imr4;
+
+            //hacemos la suma
+            imr = im1 + im2;
+            imr1 = im1 + im2 + imr;
+            imr2 = imr - im1;
+            imr3 = imr * imr1;
+            imr4 = imr / imr1;
+
+            Console.WriteLine("{0} + {1} = {2}", im1, im2, imr);
+            Console.WriteLine("{0} + {1} + {2} = {3}", im1, im2, imr, imr1);
+            Console.WriteLine("{0} - {1} = {2}", imr, im1, imr2);
+            Console.WriteLine("{0} * {1} = {2}", imr, im1, imr3);
+            Console.WriteLine("{0} / {1} = {2}", imr, im1, imr4);
+            Main2();
+        }
+        static void Main2()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("======");
+            CImaginario im1 = new CImaginario(1, 2);
+            CImaginario im2 = new CImaginario(3, 4);
+
+            Console.WriteLine(im1);
+            im1++;
+            Console.WriteLine(im1);
+            Console.WriteLine("-------");
+
+            Console.WriteLine(im2);
+            im2--;
+            Console.WriteLine(im2);
+            Console.WriteLine("-------");
+
+            Console.WriteLine(im1);
+            im1+=im2;
+            Console.WriteLine(im1);
+            Console.WriteLine("-------");
+
+            main3();
+            
+        }
+
+        static void main3()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("======");
+
+            CImaginario im1 = new CImaginario(1, 2);
+            CImaginario im2 = new CImaginario(3, 4);
+            CImaginario im3 = new CImaginario(1, 2);
+
+            if (im1 == im3)
+                Console.WriteLine("Son iguales");
+
+            if (im2 != im1)
+                Console.WriteLine("Son diferentes");
+
+            Console.Read();
+        }
+    }
+}
